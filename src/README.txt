5372ae0e53e26d5608aef0b88d87398f18e0d55e
Ertruby <mvandoesum@hotmail.com>
2015-07-03 23:21:03 +0200

    stuff

--- src/README.txt
+++ src/README.txt
@@ -3,3 +3,4 @@ How to compile a piece of Potato code:
 - execute Main.hs (you need haskell of course)
 - type in: compile *file path as string* --> compile "input/test0.txt"
 - open the output file in the map output to see the generated sprockel code
+- to run the generated files, use GHC/GHCI in the src folder --> GHCI "output/Test0.hs"
--- src/Testing.hs
+++ src/Testing.hs
@@ -1,1 +1,22 @@
 module Testing where
+
+import System.IO
+import System.FilePath
+import System.Directory
+import Parse (parse0, showAST)
+import Checker (testCheck, countLines)
+import TreeWalker (writeToFile)
+import Data.List
+import Data.Char
+
+        
+testSyntax :: FilePath -> IO [Char]
+testSyntax input = do
+        openFile input ReadMode
+        h <- openFile input ReadMode
+        contents <- hGetContents h
+        let ast = parse0 contents
+        let trtrn = if testCheck ast
+                        then "No syntax errors found"
+                        else "Syntax errors found"
+        return trtrn

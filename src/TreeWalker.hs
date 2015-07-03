22ffa33245cec5f8e84149259178e22f5066e23d
Ertruby <mvandoesum@hotmail.com>
2015-07-03 20:51:21 +0200

    TreeWalker comments

--- src/TreeWalker.hs
+++ src/TreeWalker.hs
@@ -7,11 +7,15 @@ import Debug.Trace
 import Data.Int
 import Sprockell.System
 
-
+-- | The 'writeToFile' function generates a string which can be written to a file.
+-- | It takes two arguments: the AST tree and the file name
 writeToFile:: [AST] -> String -> String 
-writeToFile ast s = "{-# LANGUAGE RecordWildCards #-} \nmodule Output." ++ s ++ " where \nimport Sprockell.System\n"
-    ++ "prog:: [Instruction] \nprog = " ++ show ((walkTree ast []) ++ [EndProg]) ++ "\nmain = run 1 prog >> putChar '\\n'"
+writeToFile ast s = "{-# LANGUAGE RecordWildCards #-} \nmodule Output." ++ s 
+    ++ " where \nimport Sprockell.System\nprog:: [Instruction] \nprog = " 
+    ++ show ((walkTree ast []) ++ [EndProg]) ++ "\nmain = run 1 prog >> putChar '\\n'"
 
+-- | The 'walkTree' function generates a list of SPRIL instructions
+-- | It takes two arguments: the AST tree and a list of tuples (var/task name,address)
 walkTree:: [AST] -> [(String, Address)] -> [Instruction]
 walkTree [] _ = []
 walkTree (n:ns) addrList  = case n of
@@ -100,7 +104,8 @@ walkTree (n:ns) addrList  = case n of
     
     
     
-    
+-- | The 'evalExpr' function generates a list of instructions from an expression
+-- | It takes an AST sub-tree, the list with known addresses and an output register
 evalExpr:: [AST] -> [(String, Address)] -> Reg -> [Instruction]
 evalExpr [] _ _ = []
 evalExpr (n:ns) addrList reg = case n of 
@@ -142,8 +147,9 @@ evalExpr (n:ns) addrList reg = case n of
 
 
 
-            
-arrayToIns:: [AST] -> [(String, Address)] -> String -> Int ->[Instruction]    
+-- | The 'arrayToIns' function generates instructions from an expression of type array
+-- | It takes an AST sub-tree, list of known addresses, identifier and the array size
+arrayToIns:: [AST] -> [(String, Address)] -> String -> Int -> [Instruction]    
 arrayToIns [ASTNode Array v@(l:ls)] addrList i size
     | ls == [] = (evalExpr [l] addrList RegA) ++ [Store RegA (Addr addr)] 
     | otherwise = (evalExpr [l] addrList RegA) ++ [Store RegA (Addr addr)] 
@@ -155,57 +161,80 @@ arrayToIns [ASTNode Expr ls] addrList i size = arrayToIns ls addrList i size
 arrayToIns [ASTNode Value ls] _ _ _ = []                
             
             
--- input = [Expr]            
+-- | The 'pushArgs' function generates instructions to push all arguments of a function call
+-- | It takes an AST sub-tree and the list of known addresses        
 pushArgs:: [AST] -> [(String, Address)] -> [Instruction]
 pushArgs [] addrList = []
 pushArgs (n:ns) addrList = pushArgs ns addrList ++ evalExpr [n] addrList RegA ++ [Push RegA]
     
+-- | The 'popArgs' function generates instructions to pop all arguments from the stack and declares them
+-- | It takes an AST sub-tree and a tuple of (instructions,address list) from the previous iteration
 popArgs:: [AST] -> ([Instruction],[(String, Address)]) -> ([Instruction],[(String, Address)])
 popArgs [] res = res
 popArgs ((ASTNode Arg [t,i]):ns) (ins,addrList) 
     = popArgs ns (ins ++ [Pop RegA, Store RegA (Addr addrC)], ((getIdf i, addrC):addrList))
     where 
         addrC = fromIntegral ((length addrList) :: Int) :: Int32
-        
+
+-- | The 'pushAddresses' function generates instructions to push the values of the known addresses
+-- | It takes the address to push
 pushAddresses:: Address -> [Instruction]
 pushAddresses addrC 
     | addrC == 0 = [Load (Addr addrC) RegA, Push RegA]
     | otherwise = [Load (Addr addrC) RegA, Push RegA] ++ pushAddresses (addrC-1) 
     
+-- | The 'popAddresses' function generates instructions to pop all values and store them back to
+--      their assigned address
+-- | Takes the address to store the popped value at and the register to use
 popAddresses:: Address -> Reg -> [Instruction]
 popAddresses addrC reg
     | addrC == 0 = [Pop reg, Store reg (Addr addrC)]
     | otherwise = popAddresses (addrC-1) reg ++ [Pop reg, Store reg (Addr addrC)]
     
+-- | The 'allocArray' function allocates memory addresses for an array
+-- | It takes the address list, the identifier of the array and the array length
 allocArray:: [(String, Address)] -> String -> Int -> [(String, Address)]
 allocArray addrList idf i
     | i == 0 = (idf ++ "[" ++ show(i) ++ "]", addrC):addrList
     | otherwise = (idf ++ "[" ++ show(i) ++ "]", addrC):(allocArray addrList idf (i-1))
     where 
         addrC = fromIntegral ((length addrList)+i :: Int) :: Int32
-        
+
+-- | The 'checkTypeArray' function checks whether the type is type array
+-- | It takes an AST sub-tree
 checkTypeArray:: AST -> Bool
 checkTypeArray (ASTNode Type [ASTNode TypeArray _]) = True
 checkTypeArray _ = False
 
+-- | The 'getLength' function returns the specified length of the array
+-- | It takes an AST sub-tree
 getLength:: [AST] -> Int
 getLength [ASTNode Expr [ASTNode Array ls]] = length ls
 getLength [ASTNode Expr [ASTNode Value [ASTNode Integer [ASTLeaf n]]]] = read n :: Int
-                                    
-regList = [RegA,RegB,RegC,RegD,RegE] 
+    
+regList = [RegA,RegB,RegC,RegD,RegE]
+
+-- | The 'getNextReg' function takes the next register from the given list
+-- | It takes a list of registers and the current register
 getNextReg:: [Reg] -> Reg -> Reg
 getNextReg (r:rs) t | r == t && length rs >= 1 = head rs
                     | r /= t = getNextReg rs t
                     | otherwise = regList!!0
-                                    
+   
+   
 ops = [Add,Sub,Mul,Div,Mod,Equal,NEq,Gt,GtE,Lt,LtE,And,Or,Xor,LShift,RShift]
 opsStr = ["plus", "minus", "times", "DividedBy", "modulo", "equals", "notEquals", "GreaterThan",
         "GreaterThanEq", "SmallerThan", "SmallerThanEq", "and", "or", "Xor", "LShift", "RShift"]
+        
+-- | The 'getOp' function returns the SPRIL operator associated with the given string
+-- | It takes a list of operators in the form of strings, a list of Operator and an operator string
 getOp :: [String] -> [Operator] -> String -> Operator
 getOp [] [] s = error ("operator not found" ++ show(s))
 getOp (x:xs) (y:ys) s   | s == x = y
                         | otherwise = getOp xs ys s
-                                    
+                                
+-- | The 'getIdf' function returns the identifier as a string
+-- | It takes an AST sub-tree
 getIdf:: AST -> String
 getIdf (ASTNode Idf [ASTLeaf s]) = s
 getIdf n = error ("getIdf" ++ show(n))

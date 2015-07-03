65effb23a1b4044028a4df8f511755b81da6f733
Ertruby <mvandoesum@hotmail.com>
2015-07-04 00:23:09 +0200

    fuck this

--- src/Testing.hs
+++ src/Testing.hs
@@ -4,19 +4,28 @@ import System.IO
 import System.FilePath
 import System.Directory
 import Parse (parse0, showAST)
-import Checker (testCheck, countLines)
+import Checker (testCheck, check, countLines)
 import TreeWalker (writeToFile)
 import Data.List
 import Data.Char
 
         
-testSyntax :: FilePath -> IO [Char]
+-- testSyntax :: FilePath -> IO [Char]
 testSyntax input = do
-        openFile input ReadMode
         h <- openFile input ReadMode
         contents <- hGetContents h
         let ast = parse0 contents
-        let trtrn = if testCheck ast
-                        then "No syntax errors found"
-                        else "Syntax errors found"
-        return trtrn
+        let trtrn = if testCheck ast 
+                        then "no syntactical errors found"
+                        else "syntactical errors found"
+        print trtrn
+        
+testContext input = do
+        h <- openFile input ReadMode
+        contents <- hGetContents h
+        let ast = parse0 contents
+        let trtrn = if testCheck ast 
+                        then "no syntactical errors found"
+                        else "syntactical errors found"
+        print trtrn
+        check ast
